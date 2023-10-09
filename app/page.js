368a0ddb2aef4138dfdab8b253b27f34129ac778
Anthony Ponson <antonyponson@gmail.com>
2023-10-09 20:00:45 +0530

    pokemon name fetch

--- app/page.js
+++ app/page.js
@@ -1,5 +1,26 @@
 import Image from 'next/image'
 
-export default function Home() {
-  return <div>hello</div>
+async function getData() {
+  try {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default async function Home() {
+  const pokemonData = await getData()
+  return (
+    <div>
+      <h2 className='text-center text-3xl font-semibold'>Poke Desk</h2>
+
+      {pokemonData.map((poke, i) => (
+        <div key={i}>
+          <h1>{poke.name}</h1>
+        </div>
+      ))}
+    </div>
+  )
 }
