51bae3ac5356f24e5a8b19a3f6dcb6f33c086134
Anthony Ponson <antonyponson@gmail.com>
2023-11-02 21:58:35 +0530

    scroll load animation added

--- app/components/PokemonGrid.jsx
+++ app/components/PokemonGrid.jsx
@@ -1,9 +1,9 @@
 'use client'
 import Link from 'next/link'
-import logo from '@/public/next.svg'
 import loader from '@/public/loader.png'
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 // Loader component
 
@@ -21,14 +21,17 @@ function Loader() {
 function PokemonGrid() {
   const [pokemonData, setPokemonData] = useState([])
   const [allPokemonData, setAllPokemonData] = useState([])
-  const [loading, setLoading] = useState(true) // State to track loading
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(0)
+  const [totalPages, setTotalPages] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await new Promise(resolve => setTimeout(resolve, 3000))
         const response = await fetch(
-          'https://pokeapi.co/api/v2/pokemon?limit=10',
+          `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${
+            (page - 1) * 20
+          }`,
           {
             next: {
               revalidate: 60,
@@ -50,17 +53,19 @@ function PokemonGrid() {
             }
           })
         )
-        setPokemonData(getPokemon)
-        setAllPokemonData(getPokemon) // Store all Pokémon data here
-        setLoading(false) // Set loading to false when data is fetched
+
+        setPokemonData(prevData => [...prevData, ...getPokemon])
+        setAllPokemonData(prevData => [...prevData, ...getPokemon])
+        setTotalPages(Math.ceil(data.count / 20))
+        setLoading(false)
       } catch (error) {
         console.error(error)
-        setLoading(false) // Set loading to false on error
+        setLoading(false)
       }
     }
 
     fetchData()
-  }, [])
+  }, [page])
 
   function handleSearch(searchTerm) {
     if (!searchTerm) {
@@ -73,34 +78,80 @@ function PokemonGrid() {
     }
   }
 
+  function fetchMoreData() {
+    if (page < totalPages) {
+      setPage(prevPage => prevPage + 1)
+    }
+  }
+
   return (
     <div className='w-[90%] mx-auto'>
       <h2 className='text-center text-3xl font-semibold'>Poke Desk</h2>
-      <input
-        className='text-black'
+      {/* <input
+        className='text-black border-2 border-solid border-teal-400'
         type='text'
         placeholder='Enter Pokémon Name'
         onChange={e => handleSearch(e.target.value)}
-      />
+      /> */}
+      <div class='relative rounded-md mt-3'>
+        <input
+          type='text'
+          class='w-full pl-10 pr-4 py-2 rounded-md bg-white border border-gray-300 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-base'
+          placeholder='Search...'
+          onChange={e => handleSearch(e.target.value)}
+        />
+        <div class='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+          <svg
+            class='w-5 h-5 text-gray-400'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M21 21l-4-4'
+            />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M15 11a4 4 0 11-8 0 4 4 0 018 0z'
+            />
+          </svg>
+        </div>
+      </div>
 
       {loading ? (
-        // Show the loader while data is being fetched
         <Loader />
       ) : (
-        <div className='flex flex-wrap -mx-2'>
-          {pokemonData.map((pokemon, i) => (
-            <div className='w-full sm:w-1/2 md:w-1/3 px-2' key={i}>
-              <Link className='block' href={`/${pokemon.name}`}>
-                <div className='bg-gray-200 p-2 rounded mb-4'>
-                  <h2 className='text-xl text-center text-blue-950 font-semibold'>
-                    {pokemon.name}
-                  </h2>
-                  <img src={pokemon.image} alt={pokemon.name} />
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
+        <InfiniteScroll
+          dataLength={pokemonData.length}
+          next={fetchMoreData}
+          hasMore={page < totalPages}
+          loader={<Loader />}
+        >
+          <div className='flex flex-wrap pt-10 mx-auto'>
+            {pokemonData.map((pokemon, i) => (
+              <div className='w-full sm:w-1/2 md:w-1/3 px-2' key={i}>
+                <Link className='block' href={`/${pokemon.name}`}>
+                  <div className='bg-gray-200 p-2 rounded mb-4'>
+                    <h2 className='text-xl text-center text-blue-950 font-semibold'>
+                      {pokemon.name}
+                    </h2>
+                    <img
+                      className='mx-auto'
+                      src={pokemon.image}
+                      alt={pokemon.name}
+                    />
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </InfiniteScroll>
       )}
     </div>
   )
--- app/loading.jsx
+++ app/loading.jsx
@@ -1,9 +1,9 @@
 export default function Loading() {
   return (
     <>
-      <main className='text-center'>
-        <h1 className='text-blue-600'>Loading...</h1>
-      </main>
+      <div className='flex h-screen items-center justify-center'>
+        <div className='animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-blue-500'></div>
+      </div>
     </>
   )
 }
